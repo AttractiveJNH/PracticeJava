53e8372f0655d221dd4a7c59db52c165ae188242
AttractiveJNH <141652584+AttractiveJNH@users.noreply.github.com>
2023-11-07 00:32:14 +0900

    Day1
    chapter 1

---
+++ JavaPractice/src/JavaPractice1/Day1.java
@@ -0,0 +1,64 @@
+package JavaPractice1;
+
+import java.util.Scanner;
+
+public class Day1
+{
+    // 11월 06일 시작~ Power JAVA 하루에 한 챕터 씩 해보기
+
+    //Add2 예제
+    public static void main(String args[])
+    {
+        //예제 1
+//        Scanner input = new Scanner(System.in); // 사용자로부터 입력을 받는 객체 생성
+//
+//        int x;
+//        int y;
+//        int sum;
+//
+//        System.out.println("첫 번째 숫자를 입력하세요 : ");
+//        x = input.nextInt(); // x의 값을 받는다
+//
+//        System.out.println("두 번째 숫자를 입력 하세요 :");
+//        y = input.nextInt(); // y의 값을 받는다
+//
+//        sum = x + y ; //sum 은 x와 y의 값을 합한 것이다.
+//
+//        System.out.println(sum); // sum의 값을 출력한다.
+
+
+        // 예제 2 월급의 저축액을 계산하라
+
+//        int salary; // 월급
+//        int deposit; // 저축액
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("월급을 입력 하세요"); // 입력 안내 출력
+//        salary = input.nextInt();
+//
+//        deposit = 10 * 12 * salary; // 10년 * 12달 * 월급
+//        System.out.printf("10년 동안의 저축액 : %d\n", deposit);
+
+        // 예제 3 원의 면적을 구하라
+//
+//        double radius; // 원의 반지름
+//        double area; // 원의 면적
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("원의 반지름을 입력하세요: " );
+//        radius = input.nextDouble();
+//        area = 3.14 * radius * radius;
+//
+//        System.out.println(area);
+
+
+        //
+
+
+
+
+
+    }
+
+
+}
+
